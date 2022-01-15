a2cfce40787605ec02a1e03823682d16288011e0
Toastuy <thetoastishot47@gmail.com>
2022-01-15 18:00:38 -0500

    Added interfaces, deleted test files

--- Assets/Scripts/IPlayer.cs
+++ Assets/Scripts/IPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class simon : MonoBehaviour
+public class IPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
