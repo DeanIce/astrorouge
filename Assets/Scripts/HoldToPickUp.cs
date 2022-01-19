f689137fe3154a8ac356865aac0e8e4628d711e8
JaeZee <jared.zins@yahoo.com>
2022-01-18 19:29:11 -0500

    Initial Commit
    Added a test Scene
    move camera over the two cylinders to update the textMeshPro with item names. Input not working correctly yet.

---
+++ Assets/Scripts/HoldToPickUp.cs
@@ -0,0 +1,100 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoldToPickUp : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private LayerMask layerMask;
+    [SerializeField]
+    private float pickupTime = 2f;
+    [SerializeField]
+    private RectTransform pickupImageRoot;
+    [SerializeField]
+    private Image pickupProgressImage;
+    [SerializeField]
+    private TextMeshProUGUI itemNameText;
+
+    private UpgradeItem itemBeingPickedUp;
+    private float currentPickupTimerElapsed;
+
+    private void Update()
+    {
+        SelectItemBeingPickedupFromRay();
+
+        if (HasItemTargetted()) {
+            pickupImageRoot.gameObject.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.F)) // change to work with new input system
+            {
+                IncrementPickupProgressAndTryComplete();
+            } 
+            else
+            {
+                currentPickupTimerElapsed = 0f;
+            }
+            UpdatePickupProgressImage();
+        }
+        else
+        {
+            pickupImageRoot.gameObject.SetActive(false);
+            currentPickupTimerElapsed = 0f;
+        }
+    }
+
+    private bool HasItemTargetted()
+    {
+        return itemBeingPickedUp != null;
+    }
+
+    private void IncrementPickupProgressAndTryComplete()
+    {
+        currentPickupTimerElapsed += Time.deltaTime;
+        if (currentPickupTimerElapsed >= pickupTime)
+        {
+            MoveItemToInventory();
+        }
+    }
+
+    private void UpdatePickupProgressImage() {
+        float pct = currentPickupTimerElapsed / pickupTime;
+        pickupProgressImage.fillAmount = pct;
+    }
+
+    private void SelectItemBeingPickedupFromRay() {
+        Ray ray = camera.ViewportPointToRay(Vector3.one / 2f);
+        Debug.DrawRay(ray.origin, ray.direction * 2f, Color.red);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 2f, layerMask))
+        {
+            var hitItem = hitInfo.collider.GetComponent<UpgradeItem>();
+
+            if (hitItem == null)
+            {
+                itemBeingPickedUp = null;
+                itemNameText.text = null;
+            }
+            else if (hitItem != null && hitItem != itemBeingPickedUp)
+            {
+                itemBeingPickedUp = hitItem;
+                itemNameText.text = "Pickup " + itemBeingPickedUp.gameObject.name;
+            }
+        }
+        else
+        {
+            itemBeingPickedUp = null;
+            itemNameText.text = null;
+        }
+    }
+    private void MoveItemToInventory()
+    {
+        Destroy(itemBeingPickedUp.gameObject);
+        itemBeingPickedUp = null;
+    }
+}
+
---
+++ Assets/Scripts/UpgradeItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeItem : MonoBehaviour
+{
+    // To be filled out with useful information that will tell the 
+    // game what stats are to be changed after pick up
+
+    // Be sure to change layer of item to the item layer
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
