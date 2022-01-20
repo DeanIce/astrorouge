1f39570039c123378a3e338bbab76d65b58af569
Simon Kirksey <44527417+simonksays123@users.noreply.github.com>
2022-01-19 21:53:27 -0500

    Add Sprint and Look ability to Player
    Needs play testing to see if Look is too choppy

--- Assets/Scripts/IPlayer.cs
+++ Assets/Scripts/IPlayer.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public interface IPlayer
 {
+    public Vector3 Look(Vector2 direction);
     public Vector3 Walk(Vector2 direction);
     public void Jump(InputAction.CallbackContext obj);
+    public void SprintToggle(InputAction.CallbackContext obj);
 }
