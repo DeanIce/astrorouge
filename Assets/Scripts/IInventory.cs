1c519156b8721bfa59c5fb5110307f501c09973a
allysonja <sonjamw17@gmail.com>
2022-01-21 17:32:05 -0500

    Added ability to stack items and dynamic hud
    Would probably need to fix some of the dimensions of the HUD box but it works!

--- Assets/Scripts/IInventory.cs
+++ Assets/Scripts/IInventory.cs
@@ -5,32 +5,33 @@ using UnityEngine;
 public class IInventory : MonoBehaviour
 {
    Dictionary<string, int> inventory = new Dictionary<string, int>();
-   private UIController uic;
+   private InventoryUIController uic;
 
    void Start() {
-       uic = GetComponent<UIController>();
+       uic = GetComponent<InventoryUIController>();
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.A)) {
-            AddItem("item_one");
+            Add_Item("item_one");
         } 
         if (Input.GetKeyDown(KeyCode.B)) {
-            AddItem("dagger");
+            Add_Item("dagger");
         }
     }
    
-   public bool HasItem(string item_name) {
+   public bool Has_Item(string item_name) {
        if (inventory.ContainsKey(item_name)) return true;
        else return false;
    }
 
-   public void AddItem(string item_name) {
-       if (this.HasItem(item_name)) {
-           inventory[item_name] = inventory[item_name]++;
+   public void Add_Item(string item_name) {
+       if (this.Has_Item(item_name)) {
+           inventory[item_name] = inventory[item_name] + 1;
+           uic.Update_Item(item_name, inventory[item_name]);
        } else {
            inventory.Add(item_name, 1);
+           uic.Add_Item(item_name, 1);
        }
-       uic.Add_Item(item_name);
    }
 }
