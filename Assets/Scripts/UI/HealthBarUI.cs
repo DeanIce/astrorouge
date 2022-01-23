ba72ee397cd2fef20dbb6bf25fa48756308e7321
Dennis Miller <deanicee@gmail.com>
2022-01-23 12:22:52 -0500

    Overhead health bar added

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HealthBarUI : MonoBehaviour
+{
+
+    [SerializeField] private Transform TargetFollow;
+    private VisualElement healthBar;
+    private VisualElement health;
+    private TextElement healthText;
+
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        healthBar = GetComponent<UIDocument>().rootVisualElement.Q("HealthBar");
+        health = healthBar.Q<VisualElement>("Health");
+        healthText = healthBar.Q<TextElement>("HealthText");
+
+        SetHealth(70, 100);
+        SetPosition();
+    }
+
+    public void SetPosition()
+    {
+        Vector2 newPos = RuntimePanelUtils.CameraTransformWorldToPanel(healthBar.panel, TargetFollow.position, mainCamera);
+        Vector3 adjustedPos = newPos;
+        adjustedPos.x = newPos.x - healthBar.layout.width / 2;
+        healthBar.transform.position = adjustedPos;
+    }
+
+    public void SetHealth(float hp, float maxHp)
+    {
+        healthText.text = hp + "/" + maxHp;
+        health.style.width = new StyleLength(Length.Percent((hp / maxHp) * 100));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (TargetFollow != null)
+        {
+            SetPosition();
+        }
+    }
+}
