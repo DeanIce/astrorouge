406ec834b71cb37c1aab99e93cf8cded3489435e
JaeZee <jared.zins@yahoo.com>
2022-01-27 20:45:33 -0500

    Organized and made prefabs consistent
    organized item scripts, made item prefabs consistent with each other, and gave them custom png files

--- Assets/Scripts/Item/RedItem.cs
+++ Assets/Scripts/Item/RedItem.cs
@@ -1,6 +1,7 @@
-public class HealthItem : AbstractItem
+public class RedItem : AbstractItem
 {
-    public override void ApplyStats() {
+    public override void ApplyStats()
+    {
         // applies stats to player (can also be used to give special abilities to items)
     }
 }
