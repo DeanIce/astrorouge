1a32a9046b728cf679cebbde7ecf9f033072dbf8
allysonja <sonjamw17@gmail.com>
2022-02-02 21:40:22 -0500

    Added updates to item stats for hearty meal and basic bullets

--- Assets/Scripts/Item/Item_BasicBullets.cs
+++ Assets/Scripts/Item/Item_BasicBullets.cs
@@ -2,6 +2,8 @@ public class Item_BasicBullets : AbstractItem
 {
     public override void ApplyStats()
     {
-        // applies stats to player (can also be used to give special abilities to items)
+        print("before pick up: " + PlayerStats.Instance.rangeDamageMultiplier);
+        PlayerStats.Instance.rangeDamageMultiplier *= (float) 1.2; // 20% increase
+        print("after pick up: " + PlayerStats.Instance.rangeDamageMultiplier);
     }
 }
