d3008f1392c2101a6893c786d3f7aee40d263df0
Toastuy <thetoastishot47@gmail.com>
2022-02-09 23:15:26 -0500

    Added ability to spawn clusters of stuff

--- Assets/Scripts/Planets/SpawnObjects.cs
+++ Assets/Scripts/Planets/SpawnObjects.cs
@@ -7,13 +7,16 @@ public class SpawnObjects : MonoBehaviour
 {
     // For procedurally spawning things
     public GameObject planet;
-    public GameObject[] environmentAssets;
-    public int[] numOfAsset;
     private Planets.PlanetGenerator planetGen;
     private Mesh mesh;
     private List<Vector3> vertices;
     bool ran = false;
-    public float yOffset;
+
+    // Collection of spawn objects
+    public GameObject[] environmentAssets;
+    public int[] numOfAsset;
+    public GameObject[] clusterAssets;
+    public int[] numInCluster;
 
     private void Start()
     {
@@ -29,16 +32,23 @@ public class SpawnObjects : MonoBehaviour
             // Set our vertices guaranteed non null
             vertices = planetGen.terrainMeshFilter.sharedMesh.vertices.ToList();
 
+            // Spawn all of our clusters
+            for (int j = 0; j < clusterAssets.Length; j++)
+            {
+                SpawnCluster(clusterAssets[j], numInCluster[j]);
+            }
+
+            // Spawn all of our random stuff
             for (int i = 0; i < environmentAssets.Length; i++)
             {
-                SpawnObject(vertices, environmentAssets[i], numOfAsset[i]);
+                SpawnObject(environmentAssets[i], numOfAsset[i]);
             }
             // Flag so we dont run indefinitely
             ran = true;
         }
     }
 
-    private Vector3 ObjectSpawnLocation(List<Vector3> vertices)
+    private Vector3 ObjectSpawnLocation()
     {
         int randIndex = UnityEngine.Random.Range(0, vertices.Count);
         Vector3 newLoc = vertices[randIndex];
@@ -50,7 +60,7 @@ public class SpawnObjects : MonoBehaviour
         return newLoc;
     }
 
-    private void SpawnObject(List<Vector3> vertices, GameObject objectToSpawn, int numToSpawn)
+    private void SpawnObject(GameObject objectToSpawn, int numToSpawn)
     {
         // We need this here so we can set rotation
         Vector3 spawnLocation = new Vector3();
@@ -58,7 +68,7 @@ public class SpawnObjects : MonoBehaviour
         {
             // FIX SCALE
             // Referenced from https://answers.unity.com/questions/974149/creating-objects-which-facing-center-of-a-sphere.html
-            spawnLocation = ObjectSpawnLocation(vertices);
+            spawnLocation = ObjectSpawnLocation();
             GameObject placeObject = Instantiate(objectToSpawn, spawnLocation, Quaternion.identity);
 
             // TEMP: Scale down huge assets
@@ -78,6 +88,33 @@ public class SpawnObjects : MonoBehaviour
             // Debug
             //Debug.Log("Just placed my " + i + "th " + objectToSpawn.name);
             //Debug.DrawRay(spawnLocation, transform.TransformDirection(spawnLocation));
+        }
+    }
+
+    private void SpawnCluster(GameObject objectToSpawn, int numToSpawn)
+    {
+        // This is to prevent overflow (i.e. random index is the last element in the vertex list)
+        int initialSpawnIndex = UnityEngine.Random.Range(0, vertices.Count - numToSpawn);
+        for (int i = 0; i < numToSpawn; i++)
+        {
+            GameObject placeObject = Instantiate(objectToSpawn, vertices[initialSpawnIndex + i], Quaternion.identity);
+
+            // TEMP: Scale down huge assets
+            placeObject.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+
+            // Find the center of our origin
+            placeObject.transform.LookAt(planet.transform.position);
+
+            // First orient stemming out from planet
+            // THEN randomly rotate on plane, NEED to do in this order
+            placeObject.transform.rotation = placeObject.transform.rotation * Quaternion.Euler(-90, 0, 0);
+            placeObject.transform.rotation = placeObject.transform.rotation * Quaternion.Euler(0, UnityEngine.Random.Range(0, 180), 0);
+
+            // Set Parent
+            placeObject.transform.parent = planet.transform;
+
+            // Remove vertex from list so we don't spawn 2 things at same spot
+            vertices.RemoveAt(initialSpawnIndex + i);
         }
     }
 }
