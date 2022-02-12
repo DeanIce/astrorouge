00eeb6af79fb93338488c7942b168cc64357f9a8
JaeZee <jared.zins@yahoo.com>
2022-02-12 15:45:10 -0500

    Added Radioactive Item Prefab
    Added the radioactive element prefab to items

---
+++ Assets/Scripts/Item/Item_ReadioActiveBullets.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_ReadioActiveBullets : AbstractItem
+{
+    public override void ApplyStats()
+    {
+        //TODO
+    }
+}
