9d3b3410ee315c5c2aaf8657441914526b8c80fd
Simon Kirksey <44527417+simonksays123@users.noreply.github.com>
2022-02-18 09:30:20 -0500

    Resolved issue for obtaining static reference to prefab at runtime

--- Assets/Scripts/UI/DamagePopupUI.cs
+++ Assets/Scripts/UI/DamagePopupUI.cs
@@ -1,16 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class DamagePopupUI : MonoBehaviour
 {
+    private static GameObject DamagePopupPF {
+        get { 
+            if (_damagePopupPF == null)
+                _damagePopupPF = Resources.Load("prefabs/DamagePopup", typeof(GameObject)) as GameObject;
+            return _damagePopupPF;
+        }
+    }
+    private static GameObject _damagePopupPF;
     
     public static DamagePopupUI Create(Vector3 position, int damageAmount) {
-        GameObject damagePopup = Resources.Load("prefabs/DamagePopup", GameObject) as GameObject;
-        Transform damagePopupTransform = Instantiate(damagePopup, position, Quaternion.identity);
+        GameObject damagePopupInstance = Instantiate(DamagePopupPF);
 
-        DamagePopupUI damagePopupUI = damagePopupTransform.GetComponent<DamagePopupUI>();
+        // TODO (Sonja): figure out which of the following update the position
+        damagePopupInstance.transform.position = position;
+        //damagePopupInstance.GetComponent<RectTransform>().position = position;
+
+        DamagePopupUI damagePopupUI = damagePopupInstance.GetComponent<DamagePopupUI>();
         damagePopupUI.Setup(damageAmount);
 
         return damagePopupUI;
