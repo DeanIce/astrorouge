3eab014a706ab2514025b5afd856a542a1117acf
Matt Hall <matthew349hall@hotmail.com>
2022-02-28 14:40:47 -0500

    begin LevelManager

---
+++ Assets/Scripts/Levels/LevelScriptableObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace Levels
+{
+    [CreateAssetMenu(fileName = "Level", menuName = "BaseLevel", order = 0)]
+    public class LevelScriptableObject : ScriptableObject
+    {
+        // Todo: boss levels
+        public string data;
+
+
+        public EnemyWeight[] enemies;
+        public SpawnObjects.AssetCount[] clusterAssets;
+        public SpawnObjects.AssetCount[] environmentAssets;
+
+        [Range(0, 10)] public int numOfPlanetsMin;
+
+        [Range(0, 10)] public int numOfPlanetsMax = 10;
+
+        public GameObject planetPrefab;
+
+        private bool isCreated;
+
+        /// <summary>
+        ///     Create the level's world meshes, determine asset placement, etc.
+        ///     Expensive process, should be invoked before the level is required.
+        /// </summary>
+        public void Create()
+        {
+            // Do a bunch of expensive calculations
+
+            isCreated = true;
+        }
+
+        /// <summary>
+        ///     Display the level. Must be called after Create()
+        /// </summary>
+        public void Load()
+        {
+            if (!isCreated)
+            {
+                Create();
+            }
+        }
+
+        [Serializable]
+        public class EnemyWeight
+        {
+            public GameObject prefab;
+            public float weight;
+        }
+    }
+}
--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,6 +1,89 @@
-﻿namespace Managers
+﻿using System;
+using System.Collections.Generic;
+using Levels;
+using UnityEditor;
+using UnityEngine;
+
+namespace Managers
 {
     public class LevelManager : ManagerSingleton<LevelManager>
     {
+        public List<LevelDescription> levels = new();
+
+        private int current;
+
+        public LevelDescription CurrentLevel
+        {
+            get
+            {
+                if (current >= levels.Count)
+                {
+                    current = 0;
+                }
+
+                return levels[current];
+            }
+        }
+
+
+        public void NextLevel()
+        {
+            if (current < levels.Count - 1)
+            {
+                current++;
+                CurrentLevel.levelScriptableObject.Create();
+                LOG($"Loading {CurrentLevel.displayName} level.");
+                CurrentLevel.levelScriptableObject.Load();
+            }
+        }
+
+        public void PrevLevel()
+        {
+            if (current > 0)
+            {
+                current--;
+                CurrentLevel.levelScriptableObject.Create();
+                LOG($"Loading {CurrentLevel.displayName} level.");
+                CurrentLevel.levelScriptableObject.Load();
+            }
+        }
+
+        [Serializable]
+        public class LevelDescription
+        {
+            public string displayName;
+            public LevelScriptableObject levelScriptableObject;
+        }
+
+        [CustomEditor(typeof(LevelManager))]
+        public class LevelManagerEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                var levelManager = (LevelManager) target;
+                var currentLevel = levelManager.CurrentLevel;
+                var centered = GUI.skin.label;
+                centered.alignment = TextAnchor.MiddleCenter;
+
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("<"))
+                {
+                    levelManager.PrevLevel();
+                }
+
+                EditorGUILayout.LabelField(
+                    $"{currentLevel.displayName} (level {levelManager.current})",
+                    centered
+                );
+                if (GUILayout.Button(">"))
+                {
+                    levelManager.NextLevel();
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+                base.OnInspectorGUI();
+            }
+        }
     }
 }
