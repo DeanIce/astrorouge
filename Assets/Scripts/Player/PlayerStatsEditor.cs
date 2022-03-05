b09a7201d828ec42765a558a199d25957dc94665
Simon Kirksey <44527417+simonksays123@users.noreply.github.com>
2022-03-05 12:44:36 -0500

    Refactored PlayerStatsEditor
    kudos to Matt for the suggestion

--- Assets/Scripts/Player/PlayerStatsEditor.cs
+++ Assets/Scripts/Player/PlayerStatsEditor.cs
@@ -175,119 +175,94 @@ public class PlayerStatsEditor : Editor
     {
         serializedObject.Update();
 
-        showCurrentMelee = EditorGUILayout.Foldout(showCurrentMelee, "Current Melee Stats", true);
-        if (showCurrentMelee)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(meleeAttackDelay);
-            EditorGUILayout.PropertyField(meleeBaseDamage);
-            EditorGUILayout.PropertyField(meleeDamageMultiplier);
-            EditorGUILayout.PropertyField(meleeCritChance);
-            EditorGUILayout.PropertyField(meleeCritMultiplier);
-            EditorGUILayout.PropertyField(meleeKnockbackForce);
-            EditorGUILayout.PropertyField(meleeAttackRange);
-            EditorGUI.indentLevel--;
-        }
+        showCurrentMelee = CreateFoldout(showCurrentMelee, "Current Melee Stats",
+            meleeAttackDelay,
+            meleeBaseDamage,
+            meleeDamageMultiplier,
+            meleeCritChance,
+            meleeCritMultiplier,
+            meleeKnockbackForce,
+            meleeAttackRange);
 
-        showBaseMelee = EditorGUILayout.Foldout(showBaseMelee, "Base Melee Stats", true);
-        if (showBaseMelee)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(baseMeleeAttackDelay);
-            EditorGUILayout.PropertyField(baseMeleeBaseDamage);
-            EditorGUILayout.PropertyField(baseMeleeDamageMultiplier);
-            EditorGUILayout.PropertyField(baseMeleeCritChance);
-            EditorGUILayout.PropertyField(baseMeleeCritMultiplier);
-            EditorGUILayout.PropertyField(baseMeleeKnockbackForce);
-            EditorGUILayout.PropertyField(baseMeleeAttackRange);
-            EditorGUI.indentLevel--;
-        }
+        showBaseMelee = CreateFoldout(showBaseMelee, "Base Melee Stats",
+            baseMeleeAttackDelay,
+            baseMeleeBaseDamage,
+            baseMeleeDamageMultiplier,
+            baseMeleeCritChance,
+            baseMeleeCritMultiplier,
+            baseMeleeKnockbackForce,
+            baseMeleeAttackRange);
 
-        showCurrentRange = EditorGUILayout.Foldout(showCurrentRange, "Current Range Stats", true);
-        if (showCurrentRange)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(rangeAttackDelay);
-            EditorGUILayout.PropertyField(rangeBaseDamage);
-            EditorGUILayout.PropertyField(rangeDamageMultiplier);
-            EditorGUILayout.PropertyField(rangeCritChance);
-            EditorGUILayout.PropertyField(rangeCritMultiplier);
-            EditorGUILayout.PropertyField(rangeKnockbackForce);
-            EditorGUILayout.PropertyField(rangeProjectileRange);
-            EditorGUILayout.PropertyField(rangeProjectileSpeed);
-            EditorGUI.indentLevel--;
-        }
+        showCurrentRange = CreateFoldout(showCurrentRange, "Current Range Stats",
+            rangeAttackDelay,
+            rangeBaseDamage,
+            rangeDamageMultiplier,
+            rangeCritChance,
+            rangeCritMultiplier,
+            rangeKnockbackForce,
+            rangeProjectileRange,
+            rangeProjectileSpeed);
 
-        showBaseRange = EditorGUILayout.Foldout(showBaseRange, "Base Range Stats", true);
-        if (showBaseRange)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(baseRangeAttackDelay);
-            EditorGUILayout.PropertyField(baseRangeBaseDamage);
-            EditorGUILayout.PropertyField(baseRangeDamageMultiplier);
-            EditorGUILayout.PropertyField(baseRangeCritChance);
-            EditorGUILayout.PropertyField(baseRangeCritMultiplier);
-            EditorGUILayout.PropertyField(baseRangeKnockbackForce);
-            EditorGUILayout.PropertyField(baseRangeProjectileRange);
-            EditorGUILayout.PropertyField(baseRangeProjectileSpeed);
-            EditorGUI.indentLevel--;
-        }
+        showBaseRange = CreateFoldout(showBaseRange, "Base Range Stats",
+            baseRangeAttackDelay,
+            baseRangeBaseDamage,
+            baseRangeDamageMultiplier,
+            baseRangeCritChance,
+            baseRangeCritMultiplier,
+            baseRangeKnockbackForce,
+            baseRangeProjectileRange,
+            baseRangeProjectileSpeed);
 
-        showCurrentDefense = EditorGUILayout.Foldout(showCurrentDefense, "Current Defense Stats", true);
-        if (showCurrentDefense)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(maxHealth);
-            EditorGUILayout.PropertyField(currentHealth);
-            EditorGUILayout.PropertyField(healthRegen);
-            EditorGUILayout.PropertyField(armor);
-            EditorGUILayout.PropertyField(dodgeChance);
-            EditorGUILayout.PropertyField(invincibilityDuration);
-            EditorGUI.indentLevel--;
-        }
+        showCurrentDefense = CreateFoldout(showCurrentDefense, "Current Defense Stats",
+            maxHealth,
+            currentHealth,
+            healthRegen,
+            armor,
+            dodgeChance,
+            invincibilityDuration);
 
-        showBaseDefense = EditorGUILayout.Foldout(showBaseDefense, "Base Defense Stats", true);
-        if (showBaseDefense)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(baseMaxHealth);
-            EditorGUILayout.PropertyField(baseHealthRegen);
-            EditorGUILayout.PropertyField(baseArmor);
-            EditorGUILayout.PropertyField(baseDodgeChance);
-            EditorGUILayout.PropertyField(baseInvincibilityDuration);
-            EditorGUI.indentLevel--;
-        }
+        showBaseDefense = CreateFoldout(showBaseDefense, "Base Defense Stats",
+            baseMaxHealth,
+            baseHealthRegen,
+            baseArmor,
+            baseDodgeChance,
+            baseInvincibilityDuration);
 
-        showCurrentMovement = EditorGUILayout.Foldout(showCurrentMovement, "Current Movement Stats", true);
-        if (showCurrentMovement)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(maxExtraJumps);
-            EditorGUILayout.PropertyField(jumpForce);
-            EditorGUILayout.PropertyField(extraJumpDampaner);
-            EditorGUILayout.PropertyField(movementSpeed);
-            EditorGUILayout.PropertyField(sprintMultiplier);
-            EditorGUILayout.PropertyField(dashCharges);
-            EditorGUILayout.PropertyField(dashDistance);
-            EditorGUILayout.PropertyField(dashRechargeRate);
-            EditorGUI.indentLevel--;
-        }
+        showCurrentMovement = CreateFoldout(showCurrentMovement, "Current Movement Stats",
+            maxExtraJumps,
+            jumpForce,
+            extraJumpDampaner,
+            movementSpeed,
+            sprintMultiplier,
+            dashCharges,
+            dashDistance,
+            dashRechargeRate);
 
-        showBaseMovement = EditorGUILayout.Foldout(showBaseMovement, "Base Movement Stats", true);
-        if (showBaseMovement)
-        {
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(baseMaxExtraJumps);
-            EditorGUILayout.PropertyField(baseJumpForce);
-            EditorGUILayout.PropertyField(baseExtraJumpDampaner);
-            EditorGUILayout.PropertyField(baseMovementSpeed);
-            EditorGUILayout.PropertyField(baseSprintMultiplier);
-            EditorGUILayout.PropertyField(baseDashCharges);
-            EditorGUILayout.PropertyField(baseDashDistance);
-            EditorGUILayout.PropertyField(baseDashRechargeRate);
-            EditorGUI.indentLevel--;
-        }
+        showBaseMovement = CreateFoldout(showBaseMovement, "Base Movement Stats", 
+            baseMaxExtraJumps,
+            baseJumpForce,
+            baseExtraJumpDampaner,
+            baseMovementSpeed,
+            baseSprintMultiplier,
+            baseDashCharges,
+            baseDashDistance,
+            baseDashRechargeRate);
 
         serializedObject.ApplyModifiedProperties();
+    }
+
+    private bool CreateFoldout(bool extended, string title, params SerializedProperty[] properties)
+    {
+        bool result = EditorGUILayout.Foldout(extended, title, true);
+        if (result)
+        {
+            EditorGUI.indentLevel++;
+            foreach(SerializedProperty prop in properties)
+            {
+                EditorGUILayout.PropertyField(prop);
+            }
+            EditorGUI.indentLevel--;
+        }
+        return result;
     }
 }
