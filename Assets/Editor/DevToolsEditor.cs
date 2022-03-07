db87e493251482facb74e76d880b0240c8cdb7e3
Matt Hall <matthew349hall@hotmail.com>
2022-03-07 17:50:03 -0500

    broken everything?

---
+++ Assets/Editor/DevToolsEditor.cs
@@ -0,0 +1,19 @@
+﻿using Managers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(DevTools))]
+    public class DevToolsEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            var devTools = (DevTools) target;
+
+            DevTools.drawPlanets = GUILayout.Toggle(DevTools.drawPlanets, "Generate Planets");
+            DevTools.logPlanetInfo = GUILayout.Toggle(DevTools.logPlanetInfo, "Log Planet Info");
+        }
+    }
+}
