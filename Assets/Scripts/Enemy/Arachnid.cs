d9cd0724530568910c7ad11173516b5159848446
unknown <jacobmatt1127@gmail.com>
2022-03-30 17:45:56 -0400

    asset creation

---
+++ Assets/Scripts/Enemy/Arachnid.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arachnid : BasicEnemyAgent
+{
+    private Animator animator;
+
+    public override void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        animator.SetInteger("battle", 1);
+        Dying = false;
+        base.Start();
+    }
+
+    public override IEnumerator Attack()
+    {
+        //rend.enabled = true;
+        Attacking = true;
+        animator.SetInteger("moving", 3);
+        yield return new WaitForSeconds(0.833f);
+        animator.SetInteger("moving", 0);
+        //rend.enabled = false;
+        Attacking = false;
+    }
+
+    public override void Die()
+    {
+        if (!Dying)
+        {
+            Dying = true;
+            if (Random.value < 0.5) animator.SetInteger("moving", 13);
+            else animator.SetInteger("moving", 12);
+            base.Die();
+        }
+    }
+}
