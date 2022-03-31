2887acb9f46b14896f9e605fe3a255d7fd2258ad
Simon Kirksey <44527417+simonksays123@users.noreply.github.com>
2022-03-31 12:15:25 -0400

    Added message for melee attack

--- Assets/Scripts/Player/PlayerDefault.cs
+++ Assets/Scripts/Player/PlayerDefault.cs
@@ -284,6 +284,7 @@ public class PlayerDefault : MonoBehaviour, IPlayer
         if (MeleeAttackDelay > 0) return;
         MeleeAttackDelay = PlayerStats.Instance.meleeAttackDelay;
 
+        print("Melee Attack Initialized"); //TODO (Simon): Remove when melee animation is added
         InstantaneousAttack();
     }
 
