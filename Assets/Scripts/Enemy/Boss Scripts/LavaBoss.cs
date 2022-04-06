4ee9b3bb075642f8af5076e73b37ade9c79b727f
Toastuy <thetoastishot47@gmail.com>
2022-04-06 17:49:18 -0400

    Started lava boss behavior

---
+++ Assets/Scripts/Enemy/Boss Scripts/LavaBoss.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBoss : MonoBehaviour, IBoss
+{
+    // Animation stuff
+    private Animator animator;
+
+    // Components
+    private Rigidbody rb;
+
+    // Status stuff
+    private bool dying;
+    private float health;
+    private float movementSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // May need to be GetComponentInChildren
+        animator = GetComponent<Animator>();
+        dying = false;
+    }
+
+    public void Die()
+    {
+
+    }
+}
