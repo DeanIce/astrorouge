c1362cbd6a5022926b9d92b53fae07cbb17a9342
Matt Hall <matthew349hall@hotmail.com>
2022-04-08 14:38:33 -0400

    yep

---
+++ Assets/Scripts/Levels/Lava.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Levels
+{
+    public class Lava : MonoBehaviour
+    {
+        private void OnCollisionStay(Collision collisionInfo)
+        {
+           
+        }
+    }
+}
