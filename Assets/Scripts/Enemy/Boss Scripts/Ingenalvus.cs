14982f238c905487e1295c8731955050c180ec52
Matt Hall <matthew349hall@hotmail.com>
2022-04-19 18:56:15 -0400

    adding damage layer

--- Assets/Scripts/Enemy/Boss Scripts/Ingenalvus.cs
+++ Assets/Scripts/Enemy/Boss Scripts/Ingenalvus.cs
@@ -1,22 +1,65 @@
-using System.Collections;
 using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 
 public class Ingenalvus : MonoBehaviour
 {
+    public float health = 100;
 
-    public int health = 100;
-    
-    
+    public GameObject colliderHead;
+
+    public int xpGift = 100;
+    public bool iAmAlive = true;
+
+    public List<IngenalvusCollider> weakPoints;
+
+    private Animator animator;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    /*
+     * Steps in battle:
+     *
+     * Idea A:
+     * - damage boss enough to trigger "damage show weak spots" events
+     * - when damaged a certain amount show those animations for a short time
+     * - if all get killed, die
+     *
+     *
+     * Idea B:
+     * - periodically release swarms
+     */
+
+
+    public void TakeDmg(float dmg)
+    {
+        if (health > 0)
+        {
+            EventManager.Instance.runStats.damageDealt += dmg;
+            health -= dmg;
+
+            print(health);
+            if (health <= 0f && iAmAlive) Die();
+        }
+    }
+
+    public void Die()
+    {
+        // Give XP for killing the enemy
+        PlayerStats.Instance.xp += xpGift;
+        EventManager.Instance.PlayerStatsUpdated();
+
+        iAmAlive = false;
+        EventManager.Instance.runStats.enemiesKilled++;
+        animator.SetTrigger("Die");
     }
 }
---
+++ Assets/Scripts/Enemy/Boss Scripts/IngenalvusCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class IngenalvusCollider : MonoBehaviour
+{
+    public Ingenalvus ingenalvus;
+
+    private float damage = 10f;
+
+    private LayerMask mask = LayerMask.GetMask();
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     print(other.name);
+    // }
+
+    public void PassThroughDamage(float dmg)
+    {
+    }
+}
