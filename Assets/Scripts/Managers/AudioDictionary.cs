dd025aa6453514ef725a5aba7e531208f70c570e
JustinHolderby <justinholderby@gmail.com>
2022-04-25 18:28:59 -0400

    Revise AudioDictionary.

--- Assets/Scripts/Managers/AudioDictionary.cs
+++ Assets/Scripts/Managers/AudioDictionary.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class AudioDictionary : MonoBehaviour
 {
     // Weapon Sounds
-    private AudioClip explosion1, explosion2, explosion3;
-    private AudioClip blaster1, blaster2, blaster3, blaster4, blaster5, blaster6, blaster7, blaster8;
+    public AudioClip explosion1, explosion2, explosion3;
+    public AudioClip blaster6, blaster8;
 
     // Enemy/Creature Sounds
-    private AudioClip entAttack1, entAttack2, entAttack3;
-    private AudioClip monsterGrowl1, monsterGrowl2, monsterGrowl3, monsterGrowl4, monsterGrowl5;
-    private AudioClip insectScreech1, insectScreech2;
-    private AudioClip monsterGrowlLong1, monsterGrowlLong2, monsterGrowlLong3, monsterGrowlLong4,
+    public AudioClip entAttack1, entAttack2, entAttack3;
+    public AudioClip monsterGrowl1, monsterGrowl2, monsterGrowl3, monsterGrowl4, monsterGrowl5;
+    public AudioClip insectScreech1, insectScreech2;
+    public AudioClip monsterGrowlLong1, monsterGrowlLong2, monsterGrowlLong3, monsterGrowlLong4,
                         monsterGrowlLong5, monsterGrowlLong6, monsterGrowlLong7;
-    private AudioClip monsterGrowlShort1, monsterGrowlShort2, monsterGrowlShort3, monsterGrowlShort4,
+    public AudioClip monsterGrowlShort1, monsterGrowlShort2, monsterGrowlShort3, monsterGrowlShort4,
                         monsterGrowlShort5, monsterGrowlShort6, monsterGrowlShort7, monsterGrowlShort8;
-    private AudioClip golemAttack1, golemAttack2, golemAttack3, golemAttack4;
+    public AudioClip golemAttack1, golemAttack2, golemAttack3, golemAttack4;
 
     // Player & UI Sounds
-    private AudioClip takeDamage1, takeDamage2, takeDamage3, takeDamage4, takeDamage5;
-    private AudioClip playerLevelUp, playerDeath, pickUpItem;
-    private AudioClip menuHover, menuOpen, menuClose, menuSelect;
+    public AudioClip takeDamage1, takeDamage2, takeDamage3, takeDamage4, takeDamage5;
+    public AudioClip playerLevelUp, playerDeath, pickUpItem;
+    public AudioClip menuHover, menuOpen, menuClose, menuSelect;
 
     // Game Music
-    private AudioClip musicLevel1, musicLevel2, musicLevel3;
-    private AudioClip musicBoss1, musicBoss2, musicBoss3;
-    private AudioClip musicMainMenu, musicCutscene;
+    public AudioClip musicLevel1, musicLevel2, musicLevel3;
+    public AudioClip musicBoss1, musicBoss2, musicBoss3;
+    public AudioClip musicMainMenu, musicCutscene;
+
+    private List<AudioClip> explosions;
+    private List<AudioClip> entAttacks;
+    private List<AudioClip> growls;
+    private List<AudioClip> longGrowls;
+    private List<AudioClip> shortGrowls;
+    private List<AudioClip> insectScreeches;
+    private List<AudioClip> golemAttacks;
+    private List<AudioClip> playerTakeDamage;
 
     public void LoadSounds()
     {
@@ -34,13 +43,7 @@ public class AudioDictionary : MonoBehaviour
         explosion2 = (AudioClip)Resources.Load("Audio/Sound Effects/Explosion2");
         explosion3 = (AudioClip)Resources.Load("Audio/Sound Effects/Explosion3");
 
-        //blaster1 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster1");
-        //blaster2 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster2");
-        //blaster3 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster3");
-        //blaster4 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster4");
-        //blaster5 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster5");
         blaster6 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster6");
-        //blaster7 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster7");
         blaster8 = (AudioClip)Resources.Load("Audio/Sound Effects/Shoot-Blaster8");
 
         entAttack1 = (AudioClip)Resources.Load("Audio/Sound Effects/EntAttack1");
@@ -70,6 +73,9 @@ public class AudioDictionary : MonoBehaviour
         monsterGrowlShort7 = (AudioClip)Resources.Load("Audio/Sound Effects/GrowlShort7");
         monsterGrowlShort8 = (AudioClip)Resources.Load("Audio/Sound Effects/GrowlShort8");
 
+        insectScreech1 = (AudioClip)Resources.Load("Audio/Sound Effects/InsectScreech");
+        insectScreech2 = (AudioClip)Resources.Load("Audio/Sound Effects/InsectScreech2");
+
         golemAttack1 = (AudioClip)Resources.Load("Audio/Sound Effects/RockGolemAttack1");
         golemAttack2 = (AudioClip)Resources.Load("Audio/Sound Effects/RockGolemAttack2");
         golemAttack3 = (AudioClip)Resources.Load("Audio/Sound Effects/RockGolemAttack3");
@@ -98,5 +104,100 @@ public class AudioDictionary : MonoBehaviour
         musicBoss3 = (AudioClip)Resources.Load("Audio/Music/Boss 3 Theme LOOP");
         musicMainMenu = (AudioClip)Resources.Load("Audio/Music/MainMenuMusicV3");
         musicCutscene = (AudioClip)Resources.Load("Audio/Music/Cutscene Music");
+
+        MakeLists();
+    }
+
+    public int RandomValue(int numSounds)
+    {
+        return (int) (Random.value * numSounds);
+    }
+
+    public void MakeLists()
+    {
+        explosions.Add(explosion1);
+        explosions.Add(explosion2);
+        explosions.Add(explosion3);
+
+        entAttacks.Add(entAttack1);
+        entAttacks.Add(entAttack2);
+        entAttacks.Add(entAttack3);
+
+        growls.Add(monsterGrowl1);
+        growls.Add(monsterGrowl2);
+        growls.Add(monsterGrowl3);
+        growls.Add(monsterGrowl4);
+        growls.Add(monsterGrowl5);
+
+        longGrowls.Add(monsterGrowlLong1);
+        longGrowls.Add(monsterGrowlLong2);
+        longGrowls.Add(monsterGrowlLong3);
+        longGrowls.Add(monsterGrowlLong4);
+        longGrowls.Add(monsterGrowlLong5);
+        longGrowls.Add(monsterGrowlLong6);
+        longGrowls.Add(monsterGrowlLong7);
+
+        shortGrowls.Add(monsterGrowlShort1);
+        shortGrowls.Add(monsterGrowlShort2);
+        shortGrowls.Add(monsterGrowlShort3);
+        shortGrowls.Add(monsterGrowlShort4);
+        shortGrowls.Add(monsterGrowlShort5);
+        shortGrowls.Add(monsterGrowlShort6);
+        shortGrowls.Add(monsterGrowlShort7);
+        shortGrowls.Add(monsterGrowlShort8);
+
+        insectScreeches.Add(insectScreech1);
+        insectScreeches.Add(insectScreech2);
+
+        golemAttacks.Add(golemAttack1);
+        golemAttacks.Add(golemAttack2);
+        golemAttacks.Add(golemAttack3);
+        golemAttacks.Add(golemAttack4);
+
+        playerTakeDamage.Add(takeDamage1);
+        playerTakeDamage.Add(takeDamage2);
+        playerTakeDamage.Add(takeDamage3);
+        playerTakeDamage.Add(takeDamage4);
+        playerTakeDamage.Add(takeDamage5);
+    }
+
+    public AudioClip RandomExplosion()
+    {
+        return explosions[RandomValue(explosions.Count)];
+    }
+
+    public AudioClip RandomEntAttack()
+    {
+        return entAttacks[RandomValue(entAttacks.Count)];
+    }
+
+    public AudioClip RandomGrowl()
+    {
+        return growls[RandomValue(growls.Count)];
+    }
+
+    public AudioClip RandomLongGrowl()
+    {
+        return longGrowls[RandomValue(longGrowls.Count)];
+    }
+
+    public AudioClip RandomShortGrowl()
+    {
+        return shortGrowls[RandomValue(shortGrowls.Count)];
+    }
+
+    public AudioClip RandomInsectScreech()
+    {
+        return explosions[RandomValue(explosions.Count)];
+    }
+
+    public AudioClip RandomGolemAttack()
+    {
+        return explosions[RandomValue(explosions.Count)];
+    }
+
+    public AudioClip RandomPlayerTakeDamage()
+    {
+        return explosions[RandomValue(explosions.Count)];
     }
 }
