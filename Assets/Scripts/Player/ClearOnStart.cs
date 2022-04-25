30766ccfd2da5a26c04cb7dd91640fa8daa95944
JaeZee <jared.zins@yahoo.com>
2022-04-25 18:59:03 -0400

    Enemies are removed from Spawn area

---
+++ Assets/Scripts/Player/ClearOnStart.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearOnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(clearArea());
+    }
+
+    private IEnumerator clearArea()
+    {
+        yield return new WaitForSeconds(2f);
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 7f);
+
+        foreach (Collider hitCollider in hitColliders)
+        {
+            Debug.Log(hitCollider.transform.root.name);
+            if (hitCollider.transform.root.CompareTag("enemy") || hitCollider.transform.root.CompareTag("Prop"))
+            {
+                Debug.Log("Destroying: " + hitCollider.transform.root.gameObject.name);
+                Destroy(hitCollider.transform.root.gameObject);
+            }
+        }
+    }
+}
