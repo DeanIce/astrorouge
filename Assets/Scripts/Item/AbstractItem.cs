37f505b29478630cb810299d69c5afa2b3bcb50e
unknown <jacobmatt1127@gmail.com>
2022-04-25 16:20:56 -0400

    added temporary gravity to items

--- Assets/Scripts/Item/AbstractItem.cs
+++ Assets/Scripts/Item/AbstractItem.cs
@@ -1,30 +1,13 @@
 using UnityEngine;
 using UnityEngine.UIElements;
-using Gravity;
 
 public abstract class AbstractItem : MonoBehaviour
 {
     public string itemName;
     public string itemDescription;
     public Texture2D itemIcon;
-    private float gravTimer = 0.5f;
-    private Rigidbody rb;
 
     public VisualElement visualElement;
-
-    public void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    public void Update()
-    {
-        if (gravTimer > 0)
-        {
-            DoGravity();
-            gravTimer -= Time.deltaTime;
-        }
-    }
 
     public override int GetHashCode()
     {
@@ -34,17 +17,6 @@ public abstract class AbstractItem : MonoBehaviour
     public override bool Equals(object other)
     {
         return base.Equals(other);
-    }
-
-    public void DoGravity()
-    {
-        // Gravity
-        Vector3 sumForce = GravityManager.GetGravity(transform.position, out Vector3 upAxis);
-        rb.AddForce(sumForce * Time.deltaTime);
-        Debug.DrawLine(transform.position, sumForce, Color.blue);
-
-        // Upright?
-        rb.MoveRotation(Quaternion.FromToRotation(transform.up, upAxis) * transform.rotation);
     }
 
     // applies stats to player (can also be used to give special abilities to items)
