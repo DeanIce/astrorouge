afc1990defede86c9d2711f0f3f6e5833c7becef
JaeZee <jared.zins@yahoo.com>
2022-04-25 21:11:25 -0400

    Got rid of red ball
    decreased timer too

--- Assets/Scripts/Player/ClearOnStart.cs
+++ Assets/Scripts/Player/ClearOnStart.cs
@@ -12,7 +12,7 @@ public class ClearOnStart : MonoBehaviour
 
     private IEnumerator clearArea()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, 7f);
 
